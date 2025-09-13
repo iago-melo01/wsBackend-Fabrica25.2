07084c9cfd7c1a9353aae8305c0d493f26b488ec
Iago Veras <ivdm0406@gmail.com>
2025-09-13 19:35:01 -0300

    :construction: adicionado contexto de busca

--- api/views.py
+++ api/views.py
@@ -3,16 +3,23 @@ from django.conf import settings
 from django import http
 from django import http
 import requests
+from pathlib import Path
+import os
+from dotenv import load_dotenv
 from .models import Movie
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+API_KEY = os.getenv("TMDB_API_KEY")
 
 def home_view(request):
     return render(request, 'home.html')
 
 def buscar_filmes_view(request):
-    pesquisa = request.GET.get("q") #query 
+    pesquisa = request.GET.get("q") #query passada no form de busca
     page = int(request.GET.get("page", 1))
     params = {
-        "api_key": 'a909fa9b7f3a8af856687641b53cde62',
+        "api_key": API_KEY,# ta exposta mas jaja eu boto no .env
         "language": "pt-BR",
         "query": pesquisa,
         "page": page,
@@ -20,23 +27,20 @@ def buscar_filmes_view(request):
         "region": "BR",
                  }
     r = requests.get('https://api.themoviedb.org/3/search/movie', params=params, timeout=10)
-    dados = r.json()
+    dados = r.json() #armazena a resposta json da requisicao na variavel dados
     
     resultados = dados.get("results")
-    return render(request, 'resultados.html', {'resultados': resultados})
+    return render(request, 'resultados.html', {'resultados': resultados, 'query': pesquisa})
 
 def visualizar_filme(request, movieId):
-
-    
-    
-    if not movieId:
+    if not movieId: #se movieId nao for passado, retornar HttpBadRequest
         return http.HttpResponseBadRequest("Parâmetro 'external_id' é obrigatório.")
     
     url = f"https://api.themoviedb.org/3/movie/{movieId}"
 
     params = {
-        "api_key": 'a909fa9b7f3a8af856687641b53cde62',   
-        "language": 'pt-BR',
+        "api_key": API_KEY,   
+        "language": 'pt-BR', 
     }
 
     try:
